531a3763f5ab12d1adc8206d74c10b0322b98986
Christopher Pyke <chrisp@Christophers-MacBook-Pro-3.local>
2019-04-03 09:37:36 +0200

    Still stuck in this damn thing

---
+++ transient/history.el
@@ -0,0 +1,12 @@
+((magit-branch nil)
+ (magit-cherry-pick
+  ("--ff"))
+ (magit-commit nil)
+ (magit-dispatch nil)
+ (magit-fetch nil)
+ (magit-notes nil)
+ (magit-patch-apply nil)
+ (magit-push nil)
+ (magit-revert
+  ("--edit"))
+ (magit-submodule nil))
