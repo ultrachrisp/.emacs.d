b312f4f60a102e8832036798452c19d40f6ec7a0
Chrisp <chris.pyke@messagebird.com>
2022-01-04 08:56:29 +0100

    fix(init): new year's tweaks

--- custom.el
+++ custom.el
@@ -3,7 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(safe-local-variable-values '((eval add-hook 'after-save-hook 'org-babel-tangle 0 t))))
+ )
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
