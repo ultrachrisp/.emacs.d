24767ec157fa2ac450ce89bac379ef669b44a585
Chrisp <chris.pyke@messagebird.com>
2022-04-11 12:27:39 +0200

    build(straight): some weird issues with straight

--- custom.el
+++ custom.el
@@ -3,7 +3,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(package-selected-packages
+   '(json-mode typescript-mode emmet-mode web-mode aggressive-indent flycheck company-tabnine company-lsp company helm-projectile helm projectile magit doom-themes ace-window which-key exec-path-from-shell use-package benchmark-init)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
--- init.el
+++ init.el
@@ -5,28 +5,9 @@
 ;; right Org version, then tangle ~/.emacs.d/init.org to
 ;; ~/.emacs.d/chrisp-init.el
 
-(when (not (file-exists-p
-            (expand-file-name "~/.emacs.d/chrisp-init.el")))
-  (message "Bootstrapping init file...")
-  (defvar bootstrapping-init t)
-  (defvar bootstrap-version)
-  (let ((bootstrap-file
-         (expand-file-name "straight/repos/straight.el/bootstrap.el"
-                           user-emacs-directory))
-        (bootstrap-version 5))
-    (unless (file-exists-p bootstrap-file)
-      (with-current-buffer
-          (url-retrieve-synchronously
-           "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
-           'silent 'inhibit-cookies)
-        (goto-char (point-max))
-        (eval-print-last-sexp)))
-    (load bootstrap-file nil 'nomessage))
-
-  (straight-use-package 'org-plus-contrib)
   (require 'org)
   (org-babel-tangle-file
    (expand-file-name "~/.emacs.d/init.org")
-   (expand-file-name "~/.emacs.d/chrisp-init.el")))
+   (expand-file-name "~/.emacs.d/chrisp-init.el"))
 
 (load-file (expand-file-name "~/.emacs.d/chrisp-init.el"))
