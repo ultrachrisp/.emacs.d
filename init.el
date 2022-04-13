7458b79a4294cd2dbd70d6c002205e019040cc9c
Chrisp <chris.pyke@messagebird.com>
2022-04-13 09:38:32 +0200

    build(init): some issues with straight

--- init.el
+++ init.el
@@ -7,23 +7,6 @@
 
 (when (not (file-exists-p
             (expand-file-name "~/.emacs.d/chrisp-init.el")))
-  (message "Bootstrapping init file...")
-  (defvar bootstrapping-init t)
-  (defvar bootstrap-version)
-  (let ((bootstrap-file
-         (expand-file-name "straight/repos/straight.el/bootstrap.el"
-                           user-emacs-directory))
-        (bootstrap-version 5))
-    (unless (file-exists-p bootstrap-file)
-      (with-current-buffer
-          (url-retrieve-synchronously
-           "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
-           'silent 'inhibit-cookies)
-        (goto-char (point-max))
-        (eval-print-last-sexp)))
-    (load bootstrap-file nil 'nomessage))
-
-  (straight-use-package 'org-plus-contrib)
   (require 'org)
   (org-babel-tangle-file
    (expand-file-name "~/.emacs.d/init.org")
