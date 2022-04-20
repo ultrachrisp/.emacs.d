343ca236e69f06a17c282a784e9b258008e39c3a
Chrisp <chris.pyke@messagebird.com>
2022-04-20 15:15:18 +0200

    build(init): looking at a different format

--- init.el
+++ init.el
@@ -1,16 +1,11 @@
 ;;;  -*- lexical-binding: t; -*-
 
-;; If ~/.emacs.d/chrisp-init.el, just load it.  Otherwise, first
-;; bootstrap Straight and load Org to make sure we end up with the
-;; right Org version, then tangle ~/.emacs.d/init.org to
-;; ~/.emacs.d/chrisp-init.el
-
 ;;(when (not (file-exists-p
 ;;            (expand-file-name "~/.emacs.d/chrisp-init.el")))
-  (require 'org)
+(require 'org)
   (org-babel-tangle-file
-   (expand-file-name "~/.emacs.d/init.org")
-   (expand-file-name "~/.emacs.d/chrisp-init.el"))
+   (expand-file-name "~/.emacs.d/README.org")
+   (expand-file-name "~/.emacs.d/setup.el"))
 ;;  )
 
-(load-file (expand-file-name "~/.emacs.d/chrisp-init.el"))
+(load-file (expand-file-name "~/.emacs.d/setup.el"))
