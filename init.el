78b35fb628a41cc6d1b0d49e48a7bde3e4f43eab
Chris Pyke <info@chrispyke.com>
2025-03-06 11:03:15 +0100

    build: stripping away everything that I don't think I need

--- init.el
+++ init.el
@@ -1,32 +1,22 @@
-;; Load straight.el to manage package installation:
-(defvar bootstrap-version)
-(unless (boundp 'bootstrapping-init)
-  (let ((bootstrap-file
-         (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
-        (bootstrap-version 5))
-    (unless (file-exists-p bootstrap-file)
-      (with-current-buffer
-          (url-retrieve-synchronously
-           "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
-           'silent 'inhibit-cookies)
-        (goto-char (point-max))
-        (eval-print-last-sexp)))
-    (load bootstrap-file nil 'nomessage)))
+;;; This is all kinds of necessary
+(require 'package)
+(setq package-enable-at-startup nil)
 
-;; use-package is a macro that simplifies installing and loading packages.
-(straight-use-package 'use-package)
-;;(setq package-enable-at-startup nil)
-(setq straight-use-package-by-default t)
+;;; remove SC if you are not using sunrise commander and org if you like outdated packages
+(setq package-archives '(("melpa-stable" . "https://stable.melpa.org/packages/")
+                         ("melpa" . "https://melpa.org/packages/")
+                         ("gnu"   . "http://elpa.gnu.org/packages/")
+                         ("org"   . "https://orgmode.org/elpa/")))
+(package-initialize)
 
-;; Load org-mode early to avoid a version clash.
-(use-package org
-  :init (setq org-startup-indented t)
-  :ensure org-plus-contrib
-  :commands (org-element-map)
-  :mode (("\\.org\\'" . org-mode)))
+;;; Bootstrapping use-package
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
 
-(require 'ob-tangle)
-(org-babel-load-file (expand-file-name "~/.emacs.d/README.org"))
+;;; This is the actual config file. It is omitted if it doesn't exist so emacs won't refuse to launch.
+(when (file-readable-p "~/.emacs.d/config.org")
+  (org-babel-load-file (expand-file-name "~/.emacs.d/config.org")))
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
