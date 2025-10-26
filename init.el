0ddc7ad7934f08449667030d8b8c7d828539f4b5
Chrisp <info@chrispyke.com>
2025-10-26 13:25:15 +0100

    build: tweaks

--- init.el
+++ init.el
@@ -23,11 +23,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(ccls company doom-themes emmet-mode exec-path-from-shell flycheck
-          helm helm-projectile json-mode lsp-ui olivetti
-          treemacs-icons-dired treemacs-magit treemacs-projectile
-          typescript-mode yasnippet yasnippet-snippets))
+ '(package-selected-packages nil)
  '(warning-suppress-types '((lsp-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
