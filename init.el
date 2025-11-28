8a9becc52049354c1231faafe19efdc358eaa489
Chrisp <info@chrispyke.com>
2025-11-28 13:32:58 +0100

    build: still setting things up

--- init.el
+++ init.el
@@ -28,7 +28,13 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages nil)
+ '(package-selected-packages
+   '(apheleia ccls company consult corfu doom-themes emmet-mode
+              exec-path-from-shell flycheck helm-projectile json-mode
+              lsp-tailwindcss lsp-ui marginalia multiple-cursors
+              olivetti orderless treemacs-icons-dired treemacs-magit
+              treemacs-projectile typescript-mode vertico
+              yasnippet-snippets))
  '(warning-suppress-types '((lsp-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
