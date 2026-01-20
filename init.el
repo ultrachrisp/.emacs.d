7e6d6afd1e9acb419d4d1610cc4e57a29cb0c2a0
Chrisp <info@chrispyke.com>
2026-01-20 17:24:48 +0100

    build: setting up

--- init.el
+++ init.el
@@ -1,37 +1,16 @@
-;;; This is all kinds of necessary
-(require 'package)
+;;; Ensure Emacs loads the most recent byte-compiled files.
+(setq load-prefer-newer t)
+
+;;; Increase garbage collection threshold
+(setq gc-cons-threshold (* 100 1024 1024)
+      read-process-output-max (* 1024 1024))
+
+;;; This is all kinds of necessary 
 (setq package-enable-at-startup nil)
 
-;;; remove SC if you are not using sunrise commander and org if you like outdated packages
-(setq package-archives '(("ELPA"  . "http://tromey.com/elpa/")
-                         ("gnu"   . "http://elpa.gnu.org/packages/")
-                         ("melpa" . "https://melpa.org/packages/")
-                         ("org"   . "https://orgmode.org/elpa/")))
-(package-initialize)
+;;; Move emacs customizations to separate file
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(load custom-file) 
 
-;;; Bootstrapping use-package
-(unless (package-installed-p 'use-package)
-  (package-refresh-contents)
-  (package-install 'use-package))
-
-;;; This is the actual config file. It is omitted if it doesn't exist so emacs won't refuse to launch.
-(when (file-readable-p "~/.emacs.d/config.org")
-  (org-babel-load-file (expand-file-name "~/.emacs.d/config.org")))
-
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   '(ccls company doom-themes emmet-mode exec-path-from-shell flycheck
-          helm helm-projectile json-mode treemacs-icons-dired
-          treemacs-magit treemacs-projectile typescript-mode yasnippet
-          yasnippet-snippets))
- '(warning-suppress-types '((lsp-mode))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(require 'org)
+(org-babel-load-file (expand-file-name "config.org" user-emacs-directory))
